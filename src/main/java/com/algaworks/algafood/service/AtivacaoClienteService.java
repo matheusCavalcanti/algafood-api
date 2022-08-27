914d18d9100af3babeb859c1e5df2e0cae0b582f
Matheus Cavalcanti <cavalcanti.smatheus@gmail.com>
2022-08-27 04:57:39 -0300

    Adicionando classes de config com Beans Spring

--- src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -4,7 +4,6 @@ import com.algaworks.algafood.modelo.Cliente;
 import com.algaworks.algafood.notificacao.Notificador;
 import org.springframework.stereotype.Component;
 
-@Component
 public class AtivacaoClienteService {
 
     private Notificador notificador;
